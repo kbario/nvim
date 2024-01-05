4a08dc8f035064e3ed89512e7385b5ef173b899c
kbario <kylebario1@gmail.com>
2024-01-06 07:00:42 +0800

    feat(obsidian): ^ daily template/keymaps

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -9,10 +9,29 @@ return {
     "BufReadPre " .. vim.fn.expand "~" .. "/brain/**.md"
   },
   dependencies = {
+    {
+      "kbario/homerows.nvim",
+      opts = {
+        custom_keys = {
+          obsid = "l2t"
+        }
+      }
+    },
     "nvim-lua/plenary.nvim",
-
     -- see below for full list of optional dependencies 👇
   },
+  keys = function()
+    local ok, hrr = pcall(require, "homerows")
+    if not ok then return end
+    local hr = hrr.lazy_hr()
+    return {
+      {
+        "<leader>" .. hr.obsid .. hr.r1,
+        "<cmd> ObsidianToday<CR>",
+        "󰇈 Obsidian: Today",
+      },
+    }
+  end,
   opts = {
     workspaces = {
       {
@@ -25,7 +44,7 @@ return {
       },
     },
     templates = {
-      subdir = "templates",
+      subdir = "../templates",
       date_format = "%Y-%m-%d-%a",
       time_format = "%H:%M",
       -- substitutions = {
@@ -33,7 +52,16 @@ return {
       --     return os.date("%Y-%m-%d", os.time() - 86400)
       --   end
     },
-
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "daily.md"
+    },
     -- see below for full list of options 👇
   },
 }
